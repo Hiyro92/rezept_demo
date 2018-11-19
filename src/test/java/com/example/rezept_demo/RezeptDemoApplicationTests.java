f274492fda09b658451fc8c88d27eac6d6bbfbff
Adrian <hiyro92@gmail.com>
2018-11-19 20:40:35 +0100

    -Init Commit

---
+++ src/test/java/com/example/rezept_demo/RezeptDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.rezept_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RezeptDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
